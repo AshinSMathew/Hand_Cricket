f31cc09056c1fb16529e0b04b2350bcbc8d4a64c
Ashin Sabu Mathew <ashinmathew1010@gmail.com>
2024-11-10 14:03:47 +0530

    Improvised code

--- Hand-Cricket.py
+++ Hand-Cricket.py
@@ -1,140 +1,139 @@
 import random
-u=0
-run=0
-runs=0
-def toss():
+wickets=0
+total_bot_runs=0
+total_user_runs=0
+def tossing():
     print(' ')
     print('*********************************************************************************************************************************************************\n')
-    z=input("\tLET'S PLAY A GAME OF HAND CRICKET (PRESS ENTER TO CONTINUE)\n")
-    print('INSTRUCTIONS')
-    V=input('YOU CAN ONLY PUT ANY NUMBER BETWEEN 1 AND 6 (PRESS ENTER TO CONTINUE)\n')
-    print('TOSSING TIME\n')
-    PO=input('DO YOU CHOOSE ODD OR EVEN ')
-    if PO.lower()=='odd':
-        RO='even'
-    elif PO.lower()=='even':
-        RO='odd'
+    input("LET'S PLAY A GAME OF HAND CRICKET (PRESS ENTER TO CONTINUE)")
+    print('\nINSTRUCTIONS')
+    input('YOU CAN ONLY PUT ANY NUMBER BETWEEN 1 AND 6 (PRESS ENTER TO CONTINUE)')
+    input("\nLETS PLAY")
+    print('\nTOSSING')
+    toss=input('DO YOU CHOOSE ODD OR EVEN: ')
+    
     print()
-    ts=int(input('ENTER ANY NUMBER BETWEEN 1 AND 6 (for toss) '))
-    ra=random.randint(1,6)
-    print('MINE IS ',ra)
-    global u
-    if (ts+ra)%2==0:
-        if PO.lower()=='even':
-            print('YOU WON THE TOSS\n')
-            q0=input('DO YOU CHOOSE BATTING OR BALLING (bat/ball) ')
-            print()
-            u=int(input('HOW MANY WICKETS '))
-            if q0.lower()=='bat':
-                batandball()
-            if q0.lower()=='ball':
-                ballandbat()
-    else:
-        print('I WON THE TOSS')
-        lp=['bat','ball']
-        q1=lp[random.randint(0,1)]
-        u=random.randint(1,3)
-        print('I CHOOSE ',u,' WICKETS')
-        print('I CHOOSE TO ',q1.upper())
-        if q1.lower()=='bat':
+    ts1=int(input('ENTER ANY NUMBER BETWEEN 1 AND 6 (for toss): '))
+    ts2=random.randint(1,6)
+    print('MINE IS ',ts2)
+    
+    global wickets
+    if ((ts1+ts2)%2==0 and toss.lower()=='even') or ((ts1+ts2)%2!=0 and toss.lower()=='odd'):
+        print('YOU WON THE TOSS\n')
+        bb1=input('DO YOU CHOOSE BATTING OR BALLING (bat/ball): ')
+        print()
+        wickets=int(input('HOW MANY WICKETS: '))
+        if bb1.lower()=='bat':
+            batandball()
+        if bb1.lower()=='ball':
             ballandbat()
-        if q1.lower()=='ball':
+    else:
+        print('I WON THE TOSS\n')
+        choice_array=['bat','ball']
+        bb2=choice_array[random.randint(0,1)]
+        wickets=random.randint(1,3)
+        print('I CHOOSE ',wickets,' WICKETS')
+        print('I CHOOSE TO ',bb2.upper())
+        if bb2.lower()=='bat':
+            ballandbat()
+        if bb2.lower()=='ball':
             batandball()
 
 def batandball():
-    print(' ')
-    m=input("NOW IT'S YOUR TURN TO BAT, AND I WILL BALL (PRESS ENTER TO START THE GAME)\n")
-    global runs
-    for i in range(1,u+1):
+    print()
+    input("NOW IT'S YOUR TURN TO BAT, AND I WILL BOWL (PRESS ENTER TO START THE GAME)\n")
+    global total_user_runs
+    input("PRESS ENTER TO START BATTING")
+    for i in range(1,wickets+1):
         while True:
-            a=int(input('YOUR TURN TO BAT '))
-            if a>6:
+            run_user=int(input('YOUR INPUT: '))
+            if run_user>6:
                 n=input('YOUR INPUT IS GREATER THAN 6\n')
                 continue
-            q=random.randint(1,6)
-            print('MINE IS ',q,'\n')
-            if a==q:
-                print('YOU ARE OUT, YOU TOOK ',runs,'RUNS\n')
-                print('YOU LOST ',i,'WICKETS\n')
+            bowling_bot=random.randint(1,6)
+            print('MINE IS ',bowling_bot,'\n')
+            if run_user==bowling_bot:
+                print('YOUR WICKET DOWN.... \n')
+                print('SCOREBOARD: ',total_user_runs,'/',i)
                 break
             else:
-                runs=runs+a
+                total_user_runs+=run_user
 
                 
-    print(' ')
-    z=input("NOW IT'S MY TURN TO BAT, AND YOU WILL BALL (PRESS ENTER TO CONTINUE)\n")
-    global run
-    for b in range(1,u+1):
-        while runs>run:
-            a1=int(input('YOUR TURN TO BALL '))
-            if a1>6:
-                l=input('YOUR INPUT IS GREATER THAN 6 (PRESS ENTER TO CONTINUE)\n')
+    print()
+    input("NOW IT'S MY TURN TO BAT, AND YOU WILL BOWL (PRESS ENTER TO CONTINUE)\n")
+    global total_bot_runs
+    input("PRESS ENTER TO START BOWLING")
+    for j in range(1,wickets+1):
+        while total_user_runs > total_bot_runs:
+            bowling_user=int(input('YOUR INPUT: '))
+            if bowling_user>6:
+                input('YOUR INPUT IS GREATER THAN 6 (PRESS ENTER TO CONTINUE)\n')
                 continue
-            q1=random.randint(1,6)
-            print('MINE IS ',q1,'\n')
-            if a1==q1:
-                print('I GOT OUT, I TOOK ',run,'RUNS\n')
-                print('I LOST ',b,'WICKETS\n')
+            run_bot=random.randint(1,6)
+            print('MINE IS ',run_bot,'\n')
+            if bowling_user==run_bot:
+                print('MY WICKET DOWN.... \n')
+                print('SCOREBOARD: ',total_bot_runs,'/',j)
                 break
             else:
-                run=run+q1
+                total_bot_runs+=run_bot
     check()
     
 def ballandbat():
-    print(' ')
-    global u
-    z=input("NOW IT'S MY TURN TO BAT, AND YOU WILL BALL (PRESS ENTER TO CONTINUE)\n")
-    global run
-    for b in range(1,u+1):
+    print()
+    input("NOW IT'S MY TURN TO BAT, AND YOU WILL BOWL (PRESS ENTER TO CONTINUE)\n")
+    global total_bot_runs
+    global total_user_runs
+    input("PRESS ENTER TO START BOWLING")
+    for j in range(1,wickets+1):
         while True:
-            a1=int(input('YOUR TURN TO BALL '))
-            if a1>6:
-                l=input('YOUR INPUT IS GREATER THAN 6 (PRESS ENTER TO CONTINUE)\n')
+            bowling_user=int(input('YOUR INPUT: '))
+            if bowling_user>6:
+                input('YOUR INPUT IS GREATER THAN 6 (PRESS ENTER TO CONTINUE)\n')
                 continue
-            q1=random.randint(1,6)
-            print('MINE IS ',q1,'\n')
-            if a1==q1:
-                print('I GOT OUT, I TOOK ',run,'RUNS\n')
-                print('I LOST ',b,'WICKETS\n')
+            run_bot=random.randint(1,6)
+            print('MINE IS ',run_bot,'\n')
+            if bowling_user==run_bot:
+                print('MY WICKET DOWN.... \n')
+                print('SCOREBOARD: ',total_bot_runs,'/',j)
                 break
             else:
-                run=run+q1
-    print(' ')
-    m=input("NOW IT'S YOUR TURN TO BAT, AND I WILL BALL (PRESS ENTER TO START THE GAME)\n")
-    global runs
-    for i in range(1,u+1):
-        while run>runs:
-            print()
-            a=int(input('YOUR TURN TO BAT '))
-            if a>6:
+                total_bot_runs+=run_bot
+    
+    print()
+    input("NOW IT'S YOUR TURN TO BAT, AND I WILL BOWL (PRESS ENTER TO START THE GAME)\n")
+    input("PRESS ENTER TO START BATTING")
+    for i in range(1,wickets+1):
+        while total_bot_runs > total_user_runs:
+            run_user=int(input('YOUR INPUT: '))
+            if run_user>6:
                 n=input('YOUR INPUT IS GREATER THAN 6\n')
                 continue
-            q=random.randint(1,6)
-            print('MINE IS ',q,'\n')
-            if a==q:
-                print('YOU ARE OUT, YOU TOOK ',runs,'RUNS\n')
-                print('YOU LOST ',i,'WICKETS\n')
+            bowling_bot=random.randint(1,6)
+            print('MINE IS ',bowling_bot,'\n')
+            if run_user==bowling_bot:
+                print('YOUR WICKET DOWN.... \n')
+                print('SCOREBOARD: ',total_user_runs,'/',i)
                 break
             else:
-                runs=runs+a
+                total_user_runs+=run_user
     check()
+
 def check():
-    global run
-    global runs
-    if runs>run:
-        print('YOU TOOK ',runs)
-        print('I TOOK ',run)
+    global total_bot_runs,total_user_runs
+    print('YOU TOOK ',total_user_runs)
+    print('I TOOK ',total_bot_runs)
+
+    if total_user_runs > total_bot_runs:
         print('CONGRATS YOU WON\n')
-    elif run>runs:
-        print('YOU TOOK ',runs)
-        print('I TOOK ',run)
+    elif total_bot_runs > total_user_runs:
         print('HAHAHA..... I WON\n')
     else:
-        print('YOU TOOK ',runs)
-        print('I TOOK ',run)
         print("IT'S A TIE MATCH\n")
-    run=0
-    runs=0
-    we=input('PREES ENTER TO CONTINUE')
+
+    total_bot_runs=0
+    total_user_runs=0
+    print("GAMEOVER\n")
     print('_________________________________________________________________________________________________________________________________________________________\n')
-toss()
+tossing()
